34322854bc7a825dd4a6a057efd05344c2ec2295
Bruno Felix <bruno.felix@fuze.cc>
2024-07-14 18:15:20 -0300

    :tada: Project created

---
+++ Weather-iOS/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_iOSApp.swift
+//  Weather-iOS
+//
+//  Created by Bruno Felix on 14/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
